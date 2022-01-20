4f536a306fbdeb17889f408460e944d738a7f332
hannahf97 <46432606+hannahf97@users.noreply.github.com>
2022-01-20 18:15:51 +0900

    [ADD] SignUpActivity.kt

--- howmath/app/src/main/java/han/howcom/howmath/SignUpActivity.kt
+++ howmath/app/src/main/java/han/howcom/howmath/SignUpActivity.kt
@@ -2,12 +2,15 @@ package han.howcom.howmath
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SignUpActivity : AppCompatActivity() {
+    val signUpButton: Button = findViewById(R.id.btn_signup)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
     }
 }
